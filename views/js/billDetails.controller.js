36ab68a90cb8933dcabd3fc2d1d2d9827be331c6
Priya Suriyamurthi <priya.suriyamurthi@gmail.com>
2017-03-13 02:07:24 -0500

    feat:update and delete functionality

--- views/js/billDetails.controller.js
+++ views/js/billDetails.controller.js
@@ -1,4 +1,4 @@
-function BillController(BillService,$stateParams,$scope,$state) {
+function BillController(BillService,$stateParams,$scope,$state,$filter) {
 	var ctrl = this;
 	ctrl.shellID = $stateParams.shellID;
 	ctrl.billList = [];
@@ -6,7 +6,7 @@ function BillController(BillService,$stateParams,$scope,$state) {
 	ctrl.currentPage = 1;
 	ctrl.numPerPage = 10;
 	ctrl.maxsize = ctrl.billList.length/10;
-	console.log(ctrl.shellID);
+	ctrl.billMessage = '';
 	ctrl.getRandomColor = function(item,index) {
 		var colors = ['#3175b0','#58b358','#52bcdc',
 					  '#efa741','#d24844'];
@@ -14,31 +14,92 @@ function BillController(BillService,$stateParams,$scope,$state) {
 		if(item.color) {
 			return item.color;
 		}
-	     
 	           color += colors[index%colors.length];
 	           item.color = color;
 	           return color;	             
-   	 	}
-   	$scope.toggleModal = function(btnClicked){
+   	}
+   	ctrl.toggleModal = function(btnClicked,bill,index) {
         $scope.buttonClicked = btnClicked;
         $scope.showModal = !$scope.showModal;
+        ctrl.btnClicked = btnClicked;
+        if(btnClicked == 'Add Bill') {
+	        ctrl.createDate = new Date();
+	        ctrl.billType= 'Rent';
+	        ctrl.billMessage = '';
+	        ctrl.itemAmount = '';
+			ctrl.description = '';
+			ctrl.comments = '';
+			ctrl.CreateEdit = 'Create';
+		} else {
+			ctrl.index = index;
+			ctrl.billID = bill._id;
+			ctrl.createDate = new Date(bill.date);
+	        ctrl.billType= bill.billType;
+	        ctrl.billMessage = '';
+	        ctrl.itemAmount = bill.itemAmount;
+			ctrl.description = bill.description;
+			ctrl.comments = bill.comments;
+			ctrl.CreateEdit = 'update';
+		}
     }
-  
+  	ctrl.billModify =function() {
+  		var newBill = {};
+  		newBill.billShell_id = ctrl.shellID;
+  		newBill.person = "priya.suriyamurthi@gmail.com";
+  		newBill.itemAmount = ctrl.itemAmount;
+		newBill.billType = ctrl.billType;
+		newBill.description = ctrl.description;
+		newBill.comments = ctrl.comments;
+		newBill.date = ctrl.createDate;
+  		if(ctrl.btnClicked == 'Add Bill') {
+		BillService.create(newBill).then(function(response) 
+		{
+			ctrl.billList.push(response);
+			ctrl.billMessage ='New Bill was added';
+			ctrl.listFilter();
+		})
+		ctrl.billMessage = '';
+		} else {
+			BillService.update(newBill,ctrl.billID).then(function(response) {
+				var key = Object.keys(ctrl.billList[ctrl.index]);
+				for(var i=0;i<key.length;i++) {
+					if(newBill.hasOwnProperty([key[i]])) {
+						ctrl.billList[ctrl.index][key[i]] = newBill[key[i]];
+					} 					
+				}
+				ctrl.billMessage ='Bill was updated';
+				ctrl.listFilter();
+			})
+		}
+		ctrl.billCreate.$setPristine();
+      	ctrl.billCreate.$setUntouched();
+      	ctrl.billCreate.$submitted = false;
+
+  	}
+  	ctrl.deleteBill = function(bill,index) {
+  		BillService.remove(bill.billShell_id,bill._id).then(function(response) {
+  			ctrl.billMessage ='Bill was removed';
+  			ctrl.billList.splice(index,1);
+  			ctrl.listFilter();
+  		})
+  	}
 	ctrl.fetchBills = function() {
 		ctrl.billList.length=0;
 		BillService
 		.retrieve(this.shellID)
 		.then(function(response) {
 			ctrl.billList = response;
-			$scope.$watch('currentPage + numPerPage',function(){
-				 var begin = ((ctrl.currentPage - 1) * ctrl.numPerPage)
+			ctrl.listFilter();
+		})	
+	}
+	ctrl.listFilter = function() {
+		$scope.$watch('currentPage + numPerPage',function(){
+				var begin = ((ctrl.currentPage - 1) * ctrl.numPerPage)
 		    		,end = begin + ctrl.numPerPage;
 		    	ctrl.filteredList = ctrl.billList.slice(begin,end);
 			})
-		})
-	}
-	
-	
+		
+	} 
 	ctrl.fetchBills();
 }
 angular
--- views/js/billDetails.services.js
+++ views/js/billDetails.services.js
@@ -5,8 +5,26 @@ function BillService($http) {
 				return response.data;
 		});
 	}
+	var create = function(bill) {
+		return $http.post(API + bill.billShell_id + '/bills',bill).then(function(response) {
+				return response.data;
+		});
+	}
+	var update = function(bill,billID) {
+		return $http.put(API + bill.billShell_id + '/bills/' + billID ,bill).then(function(response) {
+			return response.data;
+		});
+	}
+	var remove = function(billShell_id,billID) {
+		return $http.delete(API + billShell_id + '/bills/' + billID ).then(function(response) {
+			return response.data;
+		});
+	}
 	return {
-		retrieve: retrieve
+		retrieve: retrieve,
+		create: create,
+		update: update,
+		remove: remove
 	}
 };
 angular
